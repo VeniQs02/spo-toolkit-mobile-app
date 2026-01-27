03d881910f28b761de44162aee1df4b260aa76c4
Jakub Kańgowski <jkangowski@9bits.pl>
2026-01-27 06:40:29 +0100

    keeping user sessionns, log out now works pt.2

--- app/src/main/java/com/example/spotoolkit/ui/UserProfile/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.spotoolkit.ui.UserProfile
-
-data class User(
-    val displayName: String,
-    val country: String,
-    val followers: Int,
-    val imageUrl: String?
-)
--- app/src/main/java/com/example/spotoolkit/util/AuthState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.spotoolkit.util
-
-sealed class AuthState {
-    data object Unauthenticated : AuthState()
-    data object Loading : AuthState()
-    data object Authenticated : AuthState()
-    data class Error(val message: String) : AuthState()
-}
